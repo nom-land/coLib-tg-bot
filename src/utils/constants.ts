cf80d71002149a4205d7c07049f9231c4b0c6099
Saltad <saltad@proton.me>
2023-11-30 15:22:57 +0800

    feat: fix reply help msg to that msg

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -17,46 +17,51 @@ ${adminCommands}
 
   2. If you want to share something, just in the group paste the URL then @ me(no permission required). e.g.
   
-  This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBookList
+  This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBook
   
   3. You can also reply to a message containing URL to make a curation(no permission required). e.g.
   
   Message A: This book is amazing!!! https://example.com/u/xyz 
-  Message B(replying to A): @${botUsername} #Romantic #AmazingBookList
-  
-  4. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
+  Message B(replying to A): @${botUsername} #Romantic #AmazingBook
   `;
+    // 4. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
     else if (mode === "group")
         return `Hi! I'm nunti.
       
-    1. If you want to share something, just in the group paste the URL then @ me(no permission required). e.g.
+    1. If you want to share something, just in the group paste the URL then @ me. e.g.
     
-    This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBookList
+    This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBook
     
-    2. You can also reply to a message containing URL to make a curation(no permission required). e.g.
+    2. You can also reply to a message containing URL to make a curation. e.g.
     
     Message A: This book is amazing!!! https://example.com/u/xyz 
-    Message B(replying to A): @${botUsername} #Romantic #AmazingBookList
-    
-    3. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
-  `;
+    Message B(replying to A): @${botUsername} #Romantic #AmazingBook
+    `;
+    // 3. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
+    //     else if (mode === "admin")
+    //         return `
+    //     Hi! I'm nunti. I can help your community have better sharing experience and help your community easily build your brand.
+    //     1. To manage your community sharing lists, you can use the following commands(admin permission required):
+    // ${adminCommands}
+    //     2. If you want to share something, just in the group paste the URL then @ me(no permission required). e.g.
+    //     This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBookList
+    //     3. You can also reply to a message containing URL to make a curation(no permission required). e.g.
+    //     Message A: This book is amazing!!! https://example.com/u/xyz
+    //     Message B(replying to A): @${botUsername} #Romantic #AmazingBookList
+    //     4. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
+    //   `;
     else if (mode === "admin")
         return `
     Hi! I'm nunti. I can help your community have better sharing experience and help your community easily build your brand.
       
-    1. To manage your community sharing lists, you can use the following commands(admin permission required):
-${adminCommands}
-  
-    2. If you want to share something, just in the group paste the URL then @ me(no permission required). e.g.
+    1. If you want to share something, just in the group paste the URL then @ me. e.g.
     
-    This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBookList
+    This book is amazing!!! https://example.com/u/xyz @${botUsername} #Romantic #AmazingBook
     
-    3. You can also reply to a message containing URL to make a curation(no permission required). e.g.
+    2. You can also reply to a message containing URL to make a curation. e.g.
     
     Message A: This book is amazing!!! https://example.com/u/xyz 
-    Message B(replying to A): @${botUsername} #Romantic #AmazingBookList
-    
-    4. Replying to any existed curation message will also be recorded with the curation together. (Welcome! Discussion is encouraged!)
+    Message B(replying to A): @${botUsername} #Romantic #AmazingBook
   `;
     return "";
 }
